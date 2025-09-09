f3e2ee75a17470d549b197bbc7774b2430594377
Koen Vossen <info@koenvossen.nl>
2025-09-09 11:58:27 +0200

    Fix path to resources

--- cdf/validators/__init__.py
+++ cdf/validators/__init__.py
@@ -1,8 +1,8 @@
-from pathlib import Path
+from importlib import resources
 
 VERSION = "0.2.1"
 
-FILES_PATH = Path("cdf", "files")
+FILES_PATH = resources.files("cdf") / "files"
 
 from .validators import (
     MetaSchemaValidator,
